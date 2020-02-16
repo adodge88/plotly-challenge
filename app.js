af669e80aa0e88a7032e512cb5090c34bc439656
adodge <Ashley.Dodge88@gmail.com>
2020-02-16 13:35:00 -0500

    removed static folder

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // BELLY BUTTON BACTERIA
 // 1. Use the D3 library to read in samples.json.
-d3.json("../samples.json").then((importedData) => {
+d3.json("data/bellyButtonData.json").then((importedData) => {
     console.log(importedData);
     // var bacteriaData = importedData;
-})
+});
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
     // Use sample_values as the values for the bar chart.
     // Use otu_ids as the labels for the bar chart.
